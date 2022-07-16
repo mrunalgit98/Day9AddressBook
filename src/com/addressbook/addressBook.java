8c8950b64e822755d1f1b6fcbe977dddc7bb06b2
mrunal <mrunalbhoir100@gmail.com>
2022-07-16 13:14:02 +0530

    UC1_Contact

--- src/com/addressbook/addressBook.java
+++ src/com/addressbook/addressBook.java
@@ -10,16 +10,9 @@ public class addressBook {
 
         System.out.println("Welcome to address Book");
         Scanner s1=new Scanner(System.in);
-        System.out.println("please enter the details of first contact(firstname,lastname,city,state and phone number)");
-        contacts c1= new contacts(s1.nextLine(),s1.nextLine(),s1.nextLine(),s1.nextLong());
-        Scanner s2=new Scanner(System.in);
-        System.out.println("Please enter the details of second contact(firstname,lastname,city,state and phone number)");
-        contacts c2= new contacts(s2.nextLine(),s2.nextLine(),s2.nextLine(),s2.nextLong());
-        Scanner s3=new Scanner(System.in);
-        System.out.println("Please enter the details of third contact(firstname,lastname,city,state and phone number)");
-        contacts c3= new contacts(s3.nextLine(),s3.nextLine(),s3.nextLine(),s3.nextLong());
-
-
+        System.out.println("Enter your firstname,lastname,city,phone number,zipcode");
+        contacts c1= new contacts(s1.nextLine(),s1.nextLine(),s1.nextLine(),s1.nextLong(),s1.nextInt());
+        
         }
 
 
--- src/com/addressbook/contacts.java
+++ src/com/addressbook/contacts.java
@@ -8,23 +8,24 @@ import java.util.Scanner;
      String lastname;
      String city;
      long phoneNumber;
+     int  zipcode;
 
-
-    public contacts(String firstname,String lastname,String city,long phoneNumber)
+    public contacts(String firstname,String lastname,String city,long phoneNumber,int zipcode)
      {
          System.out.println("The Contact Details are as follows");
          this.firstname=firstname;
          this.lastname=lastname;
          this.city=city;
          this.phoneNumber=phoneNumber;
+         this.zipcode=zipcode;
 
          System.out.println("Name of the contact is : "+this.firstname+" "+this.lastname);
          System.out.println("City is "+this.city);
          System.out.println("Phone number= "+this.phoneNumber);
+         System.out.println("zipcode= "+this.zipcode);
 
      }
 
 
 
 }
-
